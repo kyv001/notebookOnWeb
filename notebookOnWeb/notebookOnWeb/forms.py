6063016c0224f20a7cc8a036fa424c3840ede6e4
杨韬 <3293972019@qq.com>
2020-04-22 09:14:56 +0800

    add-database-sucessful

--- notebookOnWeb/notebookOnWeb/forms.py
+++ notebookOnWeb/notebookOnWeb/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
-from wtforms.validators import Length,DataRequired
+from wtforms.validators import Length,DataRequired,EqualTo
 
 class loginForm(FlaskForm):
     username = StringField(
@@ -18,3 +18,28 @@ class loginForm(FlaskForm):
         ]
     )
     submit = SubmitField("登录")
+
+class logonForm(FlaskForm):
+    username = StringField(
+        "用户名：",
+        validators=[
+            Length(min=2,max=10,message="请输入2~10个字符长度的用户名"),
+            DataRequired(message="用户名不能为空！")
+        ]
+    )
+    userpass = PasswordField(
+        "密码：",
+        validators=[
+            Length(min=4,max=12,message="请输入4~12个字符长度的密码"),
+            DataRequired(message="密码不能为空！")
+        ]
+    )
+    passagain = PasswordField(
+        "重新输入密码：",
+        validators=[
+            Length(min=4,max=12,message="请输入4~12个字符长度的密码"),
+            DataRequired(message="密码不能为空！"),
+            EqualTo("userpass","两次密码不同！")
+        ]
+    )
+    submit = SubmitField("注册")
--- notebookOnWeb/notebookOnWeb/views.py
+++ notebookOnWeb/notebookOnWeb/views.py
@@ -7,6 +7,21 @@ import sqlite3
 from notebookOnWeb.forms import *
 from notebookOnWeb import app
 
+conn = sqlite3.connect("data.db",check_same_thread=False)
+cursor = conn.cursor()
+
+try:
+    cursor.execute(
+        """
+        CREATE TABLE users(
+            name        CHAR(10) PRIMARY KEY    NOT NULL,
+            password    CHAR(12)                NOT NULL
+        );
+        """
+    )
+    conn.commit()
+except:...
+
 @app.route('/home')
 def home():
     return render_template(
@@ -24,9 +39,25 @@ def toHome(): # go back home
 def login():
     loginform = loginForm()
     if loginform.validate_on_submit():
-        session['usr'] = loginform.username.data
-        return redirect(url_for("home"))
+        cursor.execute("SELECT password FROM users WHERE name = ?",(loginform.username.data,))
+        if cursor.fetchone()[0] == loginform.userpass.data:
+            session['usr'] = loginform.username.data
+            return redirect(url_for("home"))
+        else:
+            return render_template('login.html',title='密码错误，请重新登陆',form=loginform)
     return render_template('login.html',title='登录',form=loginform)
+
+@app.route('/logon',methods=['GET','POST'])
+def logon():
+    logonform = logonForm()
+    if logonform.validate_on_submit():
+        session['usr'] = logonform.username.data
+        cursor.execute("INSERT INTO users VALUES (?,?)",(
+            logonform.username.data,
+            logonform.userpass.data))
+        conn.commit()
+        return redirect(url_for("home"))
+    return render_template('logon.html',title='注册',form=logonform)
 
 @app.route('/logout',methods=['GET','POST'])
 def logout():
